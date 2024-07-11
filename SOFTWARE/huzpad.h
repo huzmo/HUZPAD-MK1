3adbea26aafdeb60898ea6c8b2097ae3c38dc064
Muhammad Huzaifa <104165657+aaGouf@users.noreply.github.com>
2024-07-11 14:11:30 -0600

    Update huzpad.h

--- SOFTWARE/huzpad.h
+++ SOFTWARE/huzpad.h
@@ -1,1 +1,21 @@
+#ifndef HUZPAD_H
+#define HUZPAD_H
+#include <Arduino.h>
+class macropad {
+public:
+  macropad();
+  void change_volume();
+  void macro(int key_num);
+  void read_input();
 
+private:
+  String user_settings[19];
+  int switch_A;                                      
+  int switch_B;
+  int switch_C;
+  int switch_D;
+  int switch_E;
+  int pot_A;
+};
+
+#endif
